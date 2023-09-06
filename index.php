e2598c517e12423b35f5217b769ac2a923fced3a
unknown <samolazov.roman.alex@gmail.com>
2023-09-06 21:47:04 +0300

    ADD: forms and table

--- index.php
+++ index.php
@@ -14,13 +14,74 @@
         <main class="app-main flex mx-auto xl:w-3/4">
             <section class="app-filters xl:w-1/4">
                 <h2 class="title_section text-3xl text-center">Filters</h2>
-                <form action="" method="post" class="form-filter">
+                <form method="post" class="form-filter">
+                    <div class="input-wrap flex flex-col mb-2">
+                        <label for="fullArcticle" class="mb-2">Arcticle</label>
+                        <input type="text" name="fullArcticle" id="fullArcticle" class="border w-3/4">
+                    </div>
+                    <div class="input-wrap flex flex-col mb-2">
+                        <label for="firstArcticle" class="mb-2">The first simbols of article</label>
+                        <input type="text" name="firstArcticle" id="firstArcticle" class="border w-3/4">
+                    </div>
+                    <div class="input-wrap flex flex-col mb-2">
+                        <label for="lastArcticle" class="mb-2">The last simbols of article</label>
+                        <input type="text" name="lastArcticle" id="lastArcticle" class="border w-3/4">
+                    </div>
+                    <div class="input-wrap flex flex-col mb-2">
+                        <label for="moreArcticle" class="mb-2">More article</label>
+                        <input type="text" name="moreArcticle" id="moretArcticle" class="border w-3/4">
+                    </div>
+                    <div class="input-wrap flex flex-col mb-2">
+                        <label for="lessArcticle" class="mb-2">Less article</label>
+                        <input type="text" name="lessArcticle" id="lesstArcticle" class="border w-3/4">
+                    </div>
+                    <div class="input-wrap flex flex-col mb-2">
+                        <label for="nameProduct" class="mb-2">Name Product</label>
+                        <input type="text" name="nameProduct" id="nameProduct" class="border w-3/4">
+                    </div>
+                    <div class="input-wrap flex flex-col mb-2">
+                        <label for="priceProduct" class="mb-2">Price</label>
+                        <input type="number" name="priceProduct" id="priceProduct" class="border w-3/4">
+                    </div>
+                    <div class="input-wrap flex flex-col mb-2">
+                        <label for="minPriceProduct" class="mb-2">Min Price</label>
+                        <input type="number" name="minPriceeProduct" id="minPriceProduct" class="border w-3/4">
+                    </div>
+                    <div class="input-wrap flex flex-col mb-2">
+                        <label for="maxPriceProduct" class="mb-2">Max Price</label>
+                        <input type="number" name="maxPriceeProduct" id="maxPriceProduct" class="border w-3/4">
+                    </div>
+                    <div class="input-wrap flex flex-col mb-2">
+                        <label for="minBalanceProduct" class="mb-2">Min Balance</label>
+                        <input type="number" name="minBalanceProduct" id="minBalanceProduct" class="border w-3/4">
+                    </div>
+                    <div class="input-wrap flex flex-col mb-2">
+                        <label for="maxBalanceProduct" class="mb-2">Max Balance</label>
+                        <input type="number" name="maxBalanceProduct" id="maxBalanceProduct" class="border w-3/4">
+                    </div>
                     <button type="submit">Parsing</button>
                 </form>
-
             </section>
             <section class="app-visible-db xl:w-3/4 ">
                 <h2 class="title_section text-3xl text-center">Data Base</h2>
+                <table class="w-full mt-5">
+                    <thead>
+                        <tr>
+                            <th class="border bg-blue-200">Article</th>
+                            <th class="border bg-blue-200">Name</th>
+                            <th class="border bg-blue-200">Price</th>
+                            <th class="border bg-blue-200">Balance</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="border text-center">586786748</td>
+                            <td class="border text-center">CORDIANT_SNOW_CROSS, PW-2 75Q б/к ОШ</td>
+                            <td class="border text-center">2153</td>
+                            <td class="border text-center">8</td>
+                        </tr>
+                    </tbody>
+                </table>
             </section>
         </main>
     </div>
