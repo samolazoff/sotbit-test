abae44781e750ea05372acafdf8911e903681aa5
Samolazoff <samolazov.roman.alex@gmail.com>
2023-09-06 08:13:07 +0300

    ADD: Layout (header and main-dirt)

--- index.php
+++ index.php
@@ -3,20 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <style type="text/tailwindcss">
-        @layer utilities {
-            .content-auto {
-                content-visibility: auto;
-            }
-        }
-    </style>
-    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <link href="./dist/output.css" rel="stylesheet">
     <title>Test</title>
 </head>
 <body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="conatiner app">
+        <header class="app-header text-right mx-auto xl:w-3/4 bg-blue-500 px-5 text-white text-3xl mb-10">
+            <h1 class="logo py-2">Test</h1>
+        </header>
+        <main class="app-main flex mx-auto xl:w-3/4">
+            <section class="app-filters xl:w-1/4">
+                <h2 class="title_section text-3xl text-center">Filters</h2>
+                <form action="" method="post" class="form-filter">
+                    <button type="submit">Parsing</button>
+                </form>
+
+            </section>
+            <section class="app-visible-db xl:w-3/4 ">
+                <h2 class="title_section text-3xl text-center">Data Base</h2>
+            </section>
+        </main>
+    </div>
 </body>
 </html>
