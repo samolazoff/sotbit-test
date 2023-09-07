0c79abb0509fdd21d997f86187cfeebcda00b04b
unknown <samolazov.roman.alex@gmail.com>
2023-09-07 20:57:24 +0300

    FIX: delete PHPExcel. ADD: SimpleXLS and create table Mysql

--- index.php
+++ index.php
@@ -66,7 +66,8 @@
             <section class="app-visible-db xl:w-3/4 ">
                 <h2 class="title_section text-3xl text-center">Data Base</h2>
                 <?php 
-                    require_once "PHPExcel.php";
+                    require_once "SimpleXLS.php";
+                    use Shuchkin\SimpleXLS;
 
                     $DB_HOST='localhost';
                     $DB_USER='root';
@@ -82,18 +83,25 @@
 
                         $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
 
-                        $test = "CREATE TABLE Test (
+                        $test = "CREATE TABLE test (
                             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                            $DB_NAME VARCHAR(30) NOT NULL,
+                            article VARCHAR(30) NOT NULL,
                             product VARCHAR(100) NOT NULL,
                             price INT(6) NOT NULL,
                             total INT(6) NOT NULL
                             )";
                         mysqli_query($conn, $test);
 
-
+                        $xls = SimpleXLS::parse('static/db.xls');
                         
+                        $arr_db = $xls->rows();
+                        unset($arr_db[0]);
+                        foreach ($arr_db as $arr) {
+                            $row = "INSERT INTO test (article, product, price, total) VALUES ('$arr[0]', '$arr[1]', '$arr[2]', '$arr[3]')";
+                            mysqli_query($conn, $row);
+                        }
                     };
+
                 ?>
                 <form enctype="multipart/form-data" method="post" class="form-loading">
                     <div class="input-wrap flex flex-col mb-2">
